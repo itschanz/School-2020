d6e8e503919ad580f8855345a96001176cb6b842
itschanz <72245759+itschanz@users.noreply.github.com>
2020-10-11 22:43:48 -0500

    Add files via upload

---
+++ Lab1_cgriffin31/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,21 @@
+/* 
+ * Program :   Hello World
+ * Programmer: Chanz Griffin
+ * Date:       09/01/2020
+ * Description:1.8.0
+ */
+
+public class HelloWorld 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World."
+                +          "\nChanz Griffin");
+        
+        System.out.println("\nThe first Hello World program."
+                +          "\nwas written by Dennis Ritchie.");
+                                           
+    }   //end main method
+
+}   //end class HelloWorld
